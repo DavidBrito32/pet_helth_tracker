be2ab69db9bda05facdea637d1bc4b47ac9aa216
Davi Brito <125412383+DavidBrito32@users.noreply.github.com>
2023-10-21 16:05:29 -0300

    estrutura inicial

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pet_helth_tracker
+
+Desenvolvendo soluções para seu amigo de 4 patas 🐕‍🦺
---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { createGlobalStyle } from 'styled-components';
+
+export const Estilo =createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    list-style: none;
+    text-decoration: none;
+    outline: none;
+  }
+
+`;
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Estilo />
+    <App />
+  </React.StrictMode>,
+)

b73d90c861b4119cffa5fd14d32a1236cd729314
Davi Brito <125412383+DavidBrito32@users.noreply.github.com>
2023-11-09 12:18:41 -0300

    Pagina de Login

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { createGlobalStyle } from 'styled-components';
@@ -11,13 +10,24 @@ export const Estilo =createGlobalStyle`
     list-style: none;
     text-decoration: none;
     outline: none;
+    font-family: 'Playpen Sans', cursive;
+  }
+
+  :root{
+    /*       Primary Colors            */
+    --mermaid-green: #0B9D8F;
+    --midnight-mutt: #2D3E4E;
+    --tiger-orange: #FF8E4E;
+    --vanilla-fluff: #FFFD98;
+    --flamingo-furry: #D81E5B;
+    /* ------------------------------- */
   }
 
 `;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <>
     <Estilo />
     <App />
-  </React.StrictMode>,
+  </>,
 )
